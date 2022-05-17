7177754df3192dc583adf5de43d8292c54d4e592
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-05-17 12:32:21 -0300

    7)  Mensajes de conexión y desconexión - Cliente
    Se coloca un código en el front-end que se muestre cuando la pagina del client este o no este conectado al servidor del socket

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////
+// HTML Reference
+
+const lblOnline  = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+
 const socket = io();
 
 
+socket.on('connect', ()=>{
+    console.log('Conect to the server');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display  = '';
+})
+
+socket.on('disconnect', () =>{
+    console.log('Desconect to the server');
+    lblOnline.style.display  = 'none';
+    lblOffline.style.display = '';
+})
